4626df66823b36fcef0984a675555641a7336ac0
David Tang <687449+iamdtang@users.noreply.github.com>
2024-02-11 22:40:08 -0800

    Logout and static login form

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,7 @@ Route::get('/', function () {
 Route::get('/register', [RegistrationController::class, 'index'])->name('registration.index');
 Route::post('/register', [RegistrationController::class, 'register'])->name('registration.create');
 Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+Route::get('/login', [AuthController::class, 'loginForm'])->name('auth.loginForm');
+Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
